e899b003561ca63468fee9ffb1d7977151c772d3
hieund20 <hieund898@gmail.com>
2021-10-14 20:13:48 +0700

    update

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import './style.scss';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Header.propTypes = {
+
+};
+
+function Header(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Header;
